8131dd2d0598059b10db02624488e0be1ecb7571
Tomáš Kadaně <tomas.kadane@cdn77.com>
2023-05-30 22:40:40 +0200

    Initial Commit

---
+++ CatClient/CatClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatClientApp.swift
+//  CatClient
+//
+//  Created by Tomáš Kadaně on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

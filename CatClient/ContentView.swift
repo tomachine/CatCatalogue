0be431d0caf292ba944309f56527983ae2a64a0a
Tomáš Kadaně <tomas.kadane@cdn77.com>
2023-06-07 16:12:05 +0200

    Cat breeds list with basic detail

--- CatClient/ContentView.swift
+++ CatClient/ContentView.swift
@@ -7,20 +7,95 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+//let Apikey = "live_FFaDOvUzr9lavna3sSZSOXXB0wChRbYhwHzOOdS7T2m80Qcs59XqCyEzRr7qN4bc"
+
+struct CatBreed: Codable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+
+class CatBreedsViewModel: ObservableObject {
+    @Published var catBreeds: [CatBreed] = []
+    @Published var imageUrlByBreed = [String:String]()
+    
+    init() {
+        fetchData()
+        fetchImages()
+    }
+    
+    private func fetchData() {
+        guard let url = URL(string: "https://api.thecatapi.com/v1/breeds?api_key=live_FFaDOvUzr9lavna3sSZSOXXB0wChRbYhwHzOOdS7T2m80Qcs59XqCyEzRr7qN4bc") else {
+            return
         }
-        .padding()
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let data = data, error == nil else {
+                print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            //decode json
+            do {
+                let breeds = try JSONDecoder().decode([CatBreed].self, from: data)
+                DispatchQueue.main.async {
+                    self?.catBreeds = breeds
+                }
+            } catch {
+                print("Error decoding data: \(error)")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func fetchImages() {
+        print("hi")
+        catBreeds.forEach()
+        
+        
     }
 }
 
+struct ContentView: View {
+    @StateObject private var viewModel = CatBreedsViewModel()
+    
+    var body: some View {
+        Group {
+            if viewModel.catBreeds.isEmpty {
+                ProgressView("Loading...")
+            } else {
+                NavigationView {
+                    List(viewModel.catBreeds, id: \.id) { breed in
+                        NavigationLink(destination: BreedDetailView(breed: breed)) {
+                            Text(breed.name)
+                        }
+                    }
+                    .navigationTitle("Cat Breeds")
+                }
+            }
+        }
+    }
+}
+
+struct BreedDetailView: View {
+    let breed: CatBreed
+    
+    var body: some View {
+        VStack {
+            Text(breed.description)
+                .padding()
+        
+        }
+        .navigationTitle(breed.name)
+    }
+}
+
+/*
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+*/
